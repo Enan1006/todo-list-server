c910490587fbec6d318f08e3f03a08a56f3ea4ed
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-07-23 22:27:31 +0600

    set connection with mongo

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Todo app is running')
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1riwc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    await client.connect();
+    const dataCollections = client.db('todoList').collection('data');
+
+}
+run().catch(console.dir)
+
+
+app.listen(port, () => {
+    console.log(`Todo app running on ${port}`)
+})
