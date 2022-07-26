c8834ea92f682c6e904ae023baf9b7ce32870ac7
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-07-26 19:48:06 +0600

    Updated

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ require('dotenv').config();
 const port = process.env.PORT || 4000;
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Todo app is running')
-})
+});
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1riwc.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,12 +23,45 @@ async function run() {
     app.get('/todos', async (req, res) => {
         const cursor = dataCollections.find({});
         const result = await cursor.toArray();
-        res.send(result)
+        res.send(result);
     });
+    app.post('/todo', async (req, res) => {
+        const data = req.body;
+        console.log(data);
+        const result = await dataCollections.insertOne(data);
+        console.log(result)
+        res.send(result);
+    });
+    app.put('/todo/:id', async (req, res) => {
+        const id = req.params.id;
+        const data = req.body;
+        console.log(data);
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                name: data.name,
+                message: data.message,
+            },
+        };
+        const result = await dataCollections.updateOne(filter, updateDoc, options);
+        res.send(result);
+    });
+    app.delete('/todo/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const result = await dataCollections.deleteOne(filter);
+        if (result.deletedCount === 1) {
+            console.log("Data is deleted with id", id)
+        }
+        else {
+            console.log("Data is already deleted")
+        }
+        res.send(result)
+    })
 
 }
 run().catch(console.dir)
-
 
 app.listen(port, () => {
     console.log(`Todo app running on ${port}`)
